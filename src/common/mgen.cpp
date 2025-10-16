92f5d0147dab073abd1c4eb629a64a7d9a01420d
EisakuSato-kke <eisaku-sato@kke.co.jp>
2025-10-16 17:37:50 +0900

    Revert "Change the display of the host address"
    This reverts commit 86188e99e12a9538b420566e2c9b417126e074d2.

--- src/common/mgen.cpp
+++ src/common/mgen.cpp
@@ -1680,18 +1680,9 @@ bool Mgen::OnCommand(Mgen::Command cmd, const char* arg, bool override)
       }
       
     case INTERFACE:
-    {
       SetDefaultMulticastInterface(arg, override);
-
-      ProtoAddress tempAddr;
-        if (arg && tempAddr.ResolveFromString(arg))
-        {
-            // 有効なIPアドレスなら、hostaddr表示用のアドレスとしても設定する
-            SetHostAddress(tempAddr);
-        }
-
       break;
-    }
+
     case DF:
     {
         FragmentationStatus df = DF_DEFAULT;
